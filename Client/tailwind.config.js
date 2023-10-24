efa989498564c31d549b664f499321d3154d8286
Adam Ramdan <arshinks12@gmail.com>
2023-10-24 15:37:17 +0700

    Feat: build landing page

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
  content: ['./Pages/**/*.cshtml', './Views/**/*.cshtml'],
+ darkMode: 'class',
  theme: {
-  extend: {},
+  container: {
+   center: true,
+   padding: '18px',
+  },
+  extend: {
+   colors: {
+    primary: '#fafbfa',
+    secondary: '#5874a7',
+    third: '#163270',
+   },
+   fontFamily: {
+    primary: ['Jomolhari', 'serif'],
+    secondary: ['jsMath-cmmi10', 'sans-serif'],
+   },
+  },
  },
- plugins: [],
 };
