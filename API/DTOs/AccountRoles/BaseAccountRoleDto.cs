6d2cc225fcad04dfb91a47909f0227b2ff3da112
Aryo Aditya <54234979+aryoaditya@users.noreply.github.com>
2023-10-25 10:32:43 +0700

    controller: add Role and AccountRole controller (#9)

---
+++ API/DTOs/AccountRoles/BaseAccountRoleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs.AccountRoles
+{
+    public class BaseAccountRoleDto
+    {
+        public Guid AccountGuid { get; set; }
+        public Guid RoleGuid { get; set; }
+    }
+}
---
+++ API/DTOs/Roles/CreateRoleDto.cs
@@ -0,0 +1,20 @@
+﻿using API.Models;
+
+namespace API.DTOs.Roles
+{
+    public class CreateRoleDto
+    {
+        public string Name { get; set; }
+
+        // Operator konversi implisit dari CreateRoleDto ke Role
+        public static implicit operator Role(CreateRoleDto createRoleDto)
+        {
+            return new Role
+            {
+                Name = createRoleDto.Name,
+                CreatedDate = DateTime.Now,
+                ModifiedDate = DateTime.Now
+            };
+        }
+    }
+}
