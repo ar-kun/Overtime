de15d490abc7bc5f15d5f6907863ea0aeb2c1fc8
noviaputri <50208879+noviaputri@users.noreply.github.com>
2023-10-26 09:34:33 +0700

    Create Approval and PaymentDetail Controller
    Include DTO

---
+++ API/DTOs/PaymentDetails/CreatePaymentDetailDto.cs
@@ -0,0 +1,23 @@
+﻿using API.DTOs.Approvals;
+using API.Models;
+
+namespace API.DTOs.PaymentDetails
+{
+    public class CreatePaymentDetailDto
+    {
+        public Guid Guid { get; set; }
+        public string TotalPay { get; set; }
+
+        // Declares a public static implicit conversion operator that takes a CreateApprovalDto parameter and returns a Approval object.
+        public static implicit operator PaymentDetail(CreatePaymentDetailDto createPaymentDetailDto)
+        {
+            return new PaymentDetail
+            {
+                Guid = createPaymentDetailDto.Guid,
+                TotalPay = createPaymentDetailDto.TotalPay,
+                CreatedDate = DateTime.Now,
+                ModifiedDate = DateTime.Now
+            };
+        }
+    }
+}
