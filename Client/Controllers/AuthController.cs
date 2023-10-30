54027cdf4b809bb1446c7ad2af773dd01c147b39
Adam Ramdan <79040246+ar-kun@users.noreply.github.com>
2023-10-30 09:40:39 +0700

    feat: integrasi login

--- Client/Controllers/AuthController.cs
+++ Client/Controllers/AuthController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.DTOs.Accounts;
+using Client.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Client.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly IAccountRepository _accountRepository;
+
+        public AuthController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginDto login)
+        {
+            var result = await _accountRepository.Login(login);
+
+            if (result.Status == "OK")
+            {
+
+                HttpContext.Session.SetString("JWToken", result.Data.Token);
+                return RedirectToAction("Index", "Employee");
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
