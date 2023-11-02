8723ec4a26eb2c55f21150f2436605442781c735
noviaputri <50208879+noviaputri@users.noreply.github.com>
2023-11-02 16:03:37 +0700

    Custom Error Page

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@ using Client.Contracts;
 using Client.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using System.Net;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -51,6 +52,24 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Custome Error page
+app.UseStatusCodePages(async context => {
+    var response = context.HttpContext.Response;
+
+    if (response.StatusCode.Equals((int)HttpStatusCode.Unauthorized))
+    {
+        response.Redirect("/unauthorized");
+    }
+    else if (response.StatusCode.Equals((int)HttpStatusCode.NotFound))
+    {
+        response.Redirect("/notfound");
+    }
+    else if (response.StatusCode.Equals((int)HttpStatusCode.Forbidden))
+    {
+        response.Redirect("/forbidden");
+    }
+});
+
 app.UseSession();
 
 app.Use(async (context, next) =>
@@ -66,6 +85,8 @@ app.Use(async (context, next) =>
 });
 
 app.UseAuthentication();
+
+//app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
