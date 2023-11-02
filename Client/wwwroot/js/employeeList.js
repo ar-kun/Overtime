60113c37c557e7d2790176b89d8cdf3d164acb89
Aryo Aditya <aryoaditya2000@gmail.com>
2023-11-02 16:25:40 +0700

    feature: register new employee account by manager

--- Client/wwwroot/js/employeeList.js
+++ Client/wwwroot/js/employeeList.js
@@ -39,4 +39,57 @@ $(document).ready(function () {
             { "data": "salary" }
         ]
     });
+
+    // Create Employee Account
+    $("#submitEmployee").click(function () {
+        submitForm();
+        return false;
+    });
 });
+
+// Function to handle form add employee
+function submitForm() {
+    let emp = {};
+    emp.firstName = $("#firstName").val();
+    emp.lastName = $("#lastName").val();
+    emp.gender = parseInt($("#gender").val());
+    emp.email = $("#email").val();
+    emp.birthDate = $("#birthDate").val();
+    emp.hiringDate = $("#hiringDate").val();
+    emp.phoneNumber = $("#phoneNumber").val();
+    emp.salary = $("#salary").val();
+    emp.password = $("#password").val();
+    emp.managerNik = $("#managerNik").val();
+    let jsonString = JSON.stringify(emp);
+    console.log(jsonString);
+
+    $.ajax({
+        url: "https://localhost:7290/api/Employee",
+        type: "POST",
+        cache: false,
+        data: jsonString,
+        contentType: "application/json"
+    }).done((result) => {
+        // Hide Modal
+        $("#employee-modal").modal('hide');
+
+        // Refresh table
+        $('#employee-table').DataTable().ajax.reload();
+
+        // Success Alert
+        Swal.fire({
+            icon: 'success',
+            title: result.message,
+            showConfirmButton: false,
+            timer: 1300
+        });
+
+        //$("#successAlert").text(result.message).show();
+    }).fail((jqXHR, textStatus, errorThrown) => {
+        let alertMsg = jqXHR.responseJSON.message;
+
+        // Fail Alert
+        Swal.fire('Failed to create', alertMsg, 'warning');
+        //$("#add-fail").text(alertMsg).show();
+    });
+}
